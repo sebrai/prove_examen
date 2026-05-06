cd4fa608bae53936355151f0ecd488b69efd097c
sebrai <sebbranden@gmail.com>
2026-05-06 10:08:20 +0200

    started the projekt with esentials

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-# prove_examen
+# prove examen
+
+## projekt: *file uploader*
+
+### features:
+
+- *log inn*
+- *register user*
+- *upload files with file type,name,and contents stored in db*
---
+++ app.py
@@ -0,0 +1,89 @@
+from flask import Flask, render_template, request, redirect, url_for, session, flash, abort
+from werkzeug.security import generate_password_hash, check_password_hash
+import mysql.connector
+import os
+import base64
+from dotenv import load_dotenv
+
+load_dotenv()
+
+user = os.getenv("user")
+pword = os.getenv("p_word")
+app = Flask(__name__)
+app.secret_key = os.getenv("skey")
+
+# Database-tilkobling
+# bruker du Mariadb så bytter du ut mysql med mariadb. connect
+# return mariadb.connect(
+# Husk å endre host, user, password og database, slik at de er tilpasset dine instillinger 
+def get_db_connection():
+    return mysql.connector.connect(
+        host="127.0.0.1",
+        user=user,
+        password=pword,
+        database=""
+    )
+@app.route("/")
+def blank():
+    return redirect(url_for('login'))
+
+@app.route("/login", methods=["POST","GET"])
+def login():
+    if  session.get('id'):
+      return redirect(url_for('home'))
+    if request.method == "POST":
+        brukernavn = request.form['brukernavn']
+        passord = request.form['passord']
+
+        conn = get_db_connection()
+        cursor = conn.cursor(dictionary=True)
+        cursor.execute("SELECT id,name,email,password,role FROM users WHERE name=%s", (brukernavn,))
+        bruker = cursor.fetchone()
+        cursor.close()
+        conn.close()
+        if not bruker:
+            return render_template("login.html", feil_melding="wrong username or password")
+        if bruker and check_password_hash(bruker['password'], passord):
+            session['name'] = bruker['name']
+            session['id'] = bruker['id']
+            session['role'] = bruker['role']
+            session['email'] = bruker['email']
+
+            return redirect(url_for("home"))
+        else:
+            return render_template("login.html", feil_melding="wrong username or password")
+
+    return render_template("login.html")
+
+@app.route("/new_user", methods=["GET", "POST"])
+def register():
+    if request.method == "POST":
+        brukernavn = request.form['brukernavn']
+        epost = request.form['epost']
+        passord = generate_password_hash(request.form['passord'])
+
+        conn = get_db_connection()
+        cursor = conn.cursor()
+        cursor.execute("INSERT INTO users (name, email, password) VALUES (%s, %s, %s)", 
+                       (brukernavn, epost, passord))
+        conn.commit()
+        cursor.close()
+        conn.close()
+        flash("user registrert!", "success")
+        return redirect(url_for("login"))
+
+    return render_template("registrer.html")
+
+@app.route("/logout")
+def logout():
+    session.clear()
+    flash("you have logged out", "info")
+    return redirect(url_for("login"))
+
+@app.route("/home")
+def home():
+    if  not session.get('id'):
+      return redirect(url_for('login'))
+    return render_template('home.html')
+if __name__ == "__main__":
+    app.run(debug=True,host='0.0.0.0', port=5000)
