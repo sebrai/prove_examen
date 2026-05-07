179e17bd78d79e3f05eb69834ce50df728d9b421
sebrai <sebbranden@gmail.com>
2026-05-07 12:35:04 +0200

    added wiatress as in comments

--- app.py
+++ app.py
@@ -116,4 +116,9 @@ def upload():
     else:
         return render_template('upload.html')
 if __name__ == "__main__":
+
     app.run(debug=True,host='0.0.0.0', port=5000)
+    #running normaly^
+
+    #serve(app, host='0.0.0.0', port=5000)
+    #run with waitress
